4ca006f8f34037442593a8b7c7d903203cc1b80a
Koo Zheng Yu <koozhengyu@Koos-MacBook-Pro.local>
2017-08-21 00:12:17 +0800

    updated game logic to create card objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,27 @@
 // variables
-var cards = ["queen", "queen", "king", "king"];
+var cards = 
+[
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 // function for game logic
@@ -24,9 +46,10 @@ var checkForMatch = function()
 // function to flip card, store and print card flipped
 var flipCard = function(cardId)
 {
-	var cardFlipped = cards[cardId];
-	cardsInPlay.push(cardFlipped);
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 }
 
